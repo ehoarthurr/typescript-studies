b64c1283bd8ac04f21944d98ee486591fb86cefb
Arthur Danielson <arthurdanielson7@gmail.com>
2025-05-21 11:05:56 -0400

    21-05-2025 #2

--- 12-Express/src/app.ts
+++ 12-Express/src/app.ts
@@ -1,13 +1,53 @@
-// 1 - Iniciando projeto
+// 1 - iniciando projeto
 // console.log("Express + TS");
 
-// 2 - Init express
-import express from "express";
+// 2 - init express
+import express, { Request, Response } from "express";
 
 const app = express();
 
+// 3 - rota com POST
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello Express!");
+});
+
+// 3 - rota com POST
+app.post("/api/produtos", (req, res) => {
+  console.log(req.body);
+
+  res.send("Produto adicionado!");
+});
+
+// 4 - rota para todos os verbos
+app.all("/api/product/check", (req, res) => {
+  // object literals
+  const responses = {
+    POST: "Inseriu algum registro",
+    GET: "Leu algum registro",
+  };
+
+  res.send(
+    responses[req.method as keyof typeof responses] ||
+      "Não podemos realizar essa operação!"
+  );
+});
+
+// 5 - interfaces do express
+app.get("/api/interfaces", (req: Request, res: Response) => {
+  res.send("Utilizando as interfaces");
+});
+
+// 6 - enviando JSON
+app.get("/api/json", (req: Request, res: Response) => {
+  res.json({
+    name: "Shirt",
+    price: 30,
+    color: "Blue",
+    sizes: ["P", "M", "G"],
+  });
 });
 
 app.listen(3000, () => {
