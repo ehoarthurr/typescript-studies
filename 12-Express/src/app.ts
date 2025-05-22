f284dabb1078e590dcff690f50538c26469b2734
Arthur Danielson <arthurdanielson7@gmail.com>
2025-05-21 21:01:44 -0400

    21-05-2025 #3

--- 12-Express/src/app.ts
+++ 12-Express/src/app.ts
@@ -2,13 +2,20 @@
 // console.log("Express + TS");
 
 // 2 - init express
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 const app = express();
 
 // 3 - rota com POST
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+
+// 11 - middleware para todas as rotas
+function showPath(req: Request, res: Response, next: NextFunction) {
+  console.log(req.path);
+  next();
+}
+
+app.use(showPath);
 
 app.get("/", (req, res) => {
   res.send("Hello Express!");
@@ -48,6 +55,87 @@ app.get("/api/json", (req: Request, res: Response) => {
     color: "Blue",
     sizes: ["P", "M", "G"],
   });
+});
+
+// 7 - router parameters
+app.get("/api/product/:id", (req: Request, res: Response) => {
+  console.log(req.params);
+
+  const id = req.params.id;
+
+  if (id === "1") {
+    const product = {
+      id: 1,
+      name: "Boné",
+      price: 10,
+    };
+
+    res.json(product);
+    return;
+  }
+
+  res.send("Produto não encontrado!");
+});
+
+// 8 - rotas complexas
+app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
+  console.log(req.params);
+
+  const id = req.params.id;
+  const reviewId = req.params.reviewId;
+
+  res.send(`Acessando a review ${reviewId} do produto ${id}`);
+});
+
+// 9 - router handler
+function getUser(req: Request, res: Response) {
+  console.log(`Resgatando o usuário com id: ${req.params.id}`);
+
+  res.send("O usuário foi encontrado!");
+}
+
+app.get("/api/user/:id", getUser);
+
+// 10 - middleware
+function checkUser(req: Request, res: Response, next: NextFunction) {
+  if (req.params.id === "1") {
+    console.log("Pode seguir!");
+    next();
+  } else {
+    console.log("Acesso restrito");
+  }
+}
+
+app.get("/api/user/:id/access", checkUser, (req: Request, res: Response) => {
+  res.json({
+    msg: "Bem-vindo à área administrativa!",
+  });
+});
+
+// 12 - req e res com generics
+app.get(
+  "/api/user/:id/details/:name",
+  (
+    req: Request<{ id: string; name: string }>,
+    res: Response<{ status: boolean }>
+  ) => {
+    console.log(`ID: ${req.params.id}`);
+    // console.log(`Name: ${req.params.teste}`);
+    console.log(`Name: ${req.params.name}`);
+
+    res.send({
+      status: true,
+    });
+  }
+);
+
+// 13 - tratando erros
+app.get("/api/error", (req: Request, res: Response) => {
+  try {
+    throw new Error("Algo de errado!");
+  } catch (e: any) {
+    res.status(500).json({ msg: e.message });
+  }
 });
 
 app.listen(3000, () => {
